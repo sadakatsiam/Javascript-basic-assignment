f59f221b832600e6b7633b115a731613a6482bc8
Sadakat Siam <126779557+sadakatsiam@users.noreply.github.com>
2024-08-28 15:44:05 +0600

    Commit

---
+++ Basic javascript assignment/Assignment 1/script.js
@@ -0,0 +1,86 @@
+// 1. Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Volvo";
+console.log(carName);
+
+
+
+// 2. On one single line, declare three variables with the following names and values:
+
+let firstName = "John", lastName = "Doe", age = 35;
+console.log(firstName, lastName, age);
+
+
+// 3. Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y). 
+
+ x = 10;
+ y = 5;
+ x *= y;
+ console.log(x);
+
+
+// 4.  Use comments to describe the correct data type of the following variables:
+
+let length = 16; // data type: number
+let lastName2 = "Johnson"; // data type: string
+
+const z = {
+  firstName2: "John",  
+  lastName2: "Doe"
+}; // data type: object
+
+
+// 5. Execute the function named myFunction.
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+// call the function and see the output 
+console.log(myFunction())
+
+
+// 6. Create an object called person with name = John, age = 50, Then,
+//    access the object to alert("John is 50").
+
+const person = {
+  name: "John",
+  age: 50
+};
+
+alert(`${person.name} is ${person.age}`);
+
+// 7. in index.html
+
+// 8. Array Related Question 
+
+// 1. Alert the number of items in an array, using the correct Array property: 
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+// 2. Change the first item of Brand to "Ford".
+
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+  Brand[0] = "Ford";
+  console.log(Brand);
+
+
+ 
+// 9. Math Related Problems
+// 1. Use the correct Math method to create a random number.
+const num = Math.random();
+console.log(num);
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+const largest = Math.max(10, 20);
+console.log(largest);
+
+// 3. Use the correct Math method to get the square root of 9.
+const root = Math.sqrt(9);
+console.log(root);
+
+
+
+
+
